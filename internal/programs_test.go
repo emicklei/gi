cb19f229d069bfb16b96d0c94896948150a260c5
Denis Panfilov <gh@flaticols.dev>
2025-10-31 20:04:22 +0100

    Added support of pointers (#1)
    * Add switch statement support
    
    * Implement pointer support
    
    * Fix pointer printing to show hex address
    
    ---------
    
    Co-authored-by: Ernest Micklei <ernest.micklei@gmail.com>

--- internal/programs_test.go
+++ internal/programs_test.go
@@ -987,6 +987,62 @@ func main() {
 }`, "an error")
 }
 
+func TestPointerBasic(t *testing.T) {
+	testProgram(t, false, false, `package main
+func main() {
+	x := 42
+	p := &x
+	print(*p)
+}`, "42")
+}
+
+func TestPointerAssignment(t *testing.T) {
+	testProgram(t, false, false, `package main
+func main() {
+	x := 10
+	p := &x
+	*p = 20
+	print(x)
+}`, "20")
+}
+
+func TestPointerMultipleAssignments(t *testing.T) {
+	testProgram(t, false, false, `package main
+func main() {
+	x := 1
+	y := 2
+	px := &x
+	py := &y
+	*px = 100
+	*py = 200
+	print(x, y)
+}`, "100200")
+}
+
+func TestPointerToString(t *testing.T) {
+	testProgram(t, false, false, `package main
+func main() {
+	s := "hello"
+	p := &s
+	*p = "world"
+	print(s)
+}`, "world")
+}
+
+func TestPointerSwap(t *testing.T) {
+	testProgram(t, false, false, `package main
+func swap(a, b *int) {
+	temp := *a
+	*a = *b
+	*b = temp
+}
+func main() {
+	x := 5
+	y := 10
+	swap(&x, &y)
+	print(x, y)
+}`, "105")
+  
 func TestNewStandardType(t *testing.T) {
 	testProgram(t, true, true, `package main
 import "sync"
