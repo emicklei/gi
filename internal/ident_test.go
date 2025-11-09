466e55663ffa964318d854060a2c3013b29400aa
Nil Zork <ernest.micklei@gmail.com>
2025-11-09 20:45:27 +0100

    rm tst

--- internal/ident_test.go
+++ internal/ident_test.go
@@ -1,15 +1,1 @@
 package internal
-
-import (
-	"go/ast"
-	"testing"
-)
-
-func TestZeroValueOfType(t *testing.T) {
-	env := newEnvironment(nil)
-	i := Ident{Ident: &ast.Ident{Name: "string"}}
-	v := i.ZeroValue(env)
-	if v.Interface() != "" {
-		t.Fail()
-	}
-}
