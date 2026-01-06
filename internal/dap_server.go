18b2c590bd78cab2fc32687ca377c3004783324b
emicklei <ernest.micklei@gmail.com>
2026-01-06 14:20:43 +0100

    rename debug->dap

---
+++ internal/dap_server.go
@@ -0,0 +1,83 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// This file contains modified source from https://github.com/google/go-dap
+
+package internal
+
+import (
+	"bufio"
+	"io"
+	"log"
+	"net"
+
+	"github.com/google/go-dap"
+)
+
+// server starts a server that listens on a specified port
+// and blocks indefinitely. This server can accept multiple
+// client connections at the same time.
+func server(port string) error {
+	listener, err := net.Listen("tcp", ":"+port)
+	if err != nil {
+		return err
+	}
+	defer listener.Close()
+	log.Println("Started server at", listener.Addr())
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Println("Connection failed:", err)
+			continue
+		}
+		log.Println("Accepted connection from", conn.RemoteAddr())
+		// Handle multiple client connections concurrently
+		go handleConnection(conn)
+	}
+}
+
+// handleConnection handles a connection from a single client.
+// It reads and decodes the incoming data and dispatches it
+// to per-request processing goroutines. It also launches the
+// sender goroutine to send resulting messages over the connection
+// back to the client.
+func handleConnection(conn net.Conn) {
+	session := &dapSession{
+		rw:           bufio.NewReadWriter(bufio.NewReader(conn), bufio.NewWriter(conn)),
+		sendQueue:    make(chan dap.Message),
+		stopStepping: make(chan struct{}),
+	}
+	go session.sendFromQueue()
+
+	for {
+		err := session.handleRequest()
+		if err != nil {
+			if err == io.EOF {
+				log.Println("No more data to read:", err)
+				break
+			}
+			// There maybe more messages to process, but
+			// we will start with the strict behavior of only accepting
+			// expected inputs.
+			log.Fatal("Server error: ", err)
+		}
+	}
+
+	log.Println("Closing connection from", conn.RemoteAddr())
+	close(session.stopStepping)
+	session.sendWg.Wait()
+	close(session.sendQueue)
+	conn.Close()
+}
