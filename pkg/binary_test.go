d1b77765f9b0838a6651c2ac596f29af4ffbf79c
emicklei <ernest.micklei@gmail.com>
2026-03-17 11:22:10 +0100

    more coverage bin funcs

--- pkg/binary_test.go
+++ pkg/binary_test.go
@@ -1,7 +1,9 @@
 package pkg
 
 import (
+	"go/token"
 	"math"
+	"reflect"
 	"strconv"
 	"testing"
 )
@@ -333,4 +335,280 @@ func main() {
 	print(x >= y)
 }`, "true")
 	})
+}
+
+func TestBinaryFuncsSignedIntegers(t *testing.T) {
+	t.Parallel()
+	const left, right = 42, 5
+	runSignedBinFuncTests(t, "int", int(left), int(right))
+	runSignedBinFuncTests(t, "int8", int8(left), int8(right))
+	runSignedBinFuncTests(t, "int16", int16(left), int16(right))
+	runSignedBinFuncTests(t, "int32", int32(left), int32(right))
+	runSignedBinFuncTests(t, "int64", int64(left), int64(right))
+}
+
+func TestBinaryFuncsUnsignedIntegers(t *testing.T) {
+	t.Parallel()
+	const left, right = 42, 5
+	runUnsignedBinFuncTests(t, "uint", uint(left), uint(right))
+	runUnsignedBinFuncTests(t, "uint8", uint8(left), uint8(right))
+	runUnsignedBinFuncTests(t, "uint16", uint16(left), uint16(right))
+	runUnsignedBinFuncTests(t, "uint32", uint32(left), uint32(right))
+	runUnsignedBinFuncTests(t, "uint64", uint64(left), uint64(right))
+}
+
+func TestBinaryFuncsFloatTypes(t *testing.T) {
+	t.Parallel()
+	runFloatBinFuncTests(t, "float32", float32(6.5), float32(2))
+	runFloatBinFuncTests(t, "float64", 6.5, 2.0)
+}
+
+func TestBinaryFuncsStringType(t *testing.T) {
+	t.Parallel()
+	runStringBinFuncTests(t, "string", "go", "gi")
+}
+
+var integerTokens = []token.Token{
+	token.ADD,
+	token.SUB,
+	token.MUL,
+	token.QUO,
+	token.REM,
+	token.AND,
+	token.OR,
+	token.XOR,
+	token.SHL,
+	token.SHR,
+	token.AND_NOT,
+	token.EQL,
+	token.LSS,
+	token.GTR,
+	token.NEQ,
+	token.LEQ,
+	token.GEQ,
+}
+
+var floatTokens = []token.Token{
+	token.ADD,
+	token.SUB,
+	token.MUL,
+	token.QUO,
+	token.EQL,
+	token.LSS,
+	token.GTR,
+	token.NEQ,
+	token.LEQ,
+	token.GEQ,
+}
+
+var stringTokens = []token.Token{
+	token.ADD,
+	token.EQL,
+	token.LSS,
+	token.GTR,
+	token.NEQ,
+	token.LEQ,
+	token.GEQ,
+}
+
+type signedInteger interface {
+	~int | ~int8 | ~int16 | ~int32 | ~int64
+}
+
+type unsignedInteger interface {
+	~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64
+}
+
+type floatNumber interface {
+	~float32 | ~float64
+}
+
+func runSignedBinFuncTests[T signedInteger](t *testing.T, typeName string, left, right T) {
+	t.Helper()
+	t.Run(typeName, func(t *testing.T) {
+		leftTyped, rightTyped := left, right
+		for _, tok := range integerTokens {
+			want := signedOp(tok, leftTyped, rightTyped)
+			assertBinaryFunc(t, typeName, tok, leftTyped, rightTyped, want)
+		}
+	})
+}
+
+func runUnsignedBinFuncTests[T unsignedInteger](t *testing.T, typeName string, left, right T) {
+	t.Helper()
+	t.Run(typeName, func(t *testing.T) {
+		leftTyped, rightTyped := left, right
+		for _, tok := range integerTokens {
+			want := unsignedOp(tok, leftTyped, rightTyped)
+			assertBinaryFunc(t, typeName, tok, leftTyped, rightTyped, want)
+		}
+	})
+}
+
+func runFloatBinFuncTests[T floatNumber](t *testing.T, typeName string, left, right T) {
+	t.Helper()
+	t.Run(typeName, func(t *testing.T) {
+		leftTyped, rightTyped := left, right
+		for _, tok := range floatTokens {
+			want := floatOp(tok, leftTyped, rightTyped)
+			assertBinaryFunc(t, typeName, tok, leftTyped, rightTyped, want)
+		}
+	})
+}
+
+func runStringBinFuncTests(t *testing.T, typeName, left, right string) {
+	t.Helper()
+	t.Run(typeName, func(t *testing.T) {
+		for _, tok := range stringTokens {
+			want := stringOp(tok, left, right)
+			assertBinaryFunc(t, typeName, tok, left, right, want)
+		}
+	})
+}
+
+func signedOp[T signedInteger](tok token.Token, left, right T) any {
+	li := int64(left)
+	ri := int64(right)
+	switch tok {
+	case token.ADD:
+		return T(li + ri)
+	case token.SUB:
+		return T(li - ri)
+	case token.MUL:
+		return T(li * ri)
+	case token.QUO:
+		return T(li / ri)
+	case token.REM:
+		return T(li % ri)
+	case token.AND:
+		return T(li & ri)
+	case token.OR:
+		return T(li | ri)
+	case token.XOR:
+		return T(li ^ ri)
+	case token.SHL:
+		return T(li << uint64(ri))
+	case token.SHR:
+		return T(li >> uint64(ri))
+	case token.AND_NOT:
+		return T(li &^ ri)
+	case token.EQL:
+		return li == ri
+	case token.LSS:
+		return li < ri
+	case token.GTR:
+		return li > ri
+	case token.NEQ:
+		return li != ri
+	case token.LEQ:
+		return li <= ri
+	case token.GEQ:
+		return li >= ri
+	default:
+		panic("unsupported token for signed integers: " + tok.String())
+	}
+}
+
+func unsignedOp[T unsignedInteger](tok token.Token, left, right T) any {
+	lu := uint64(left)
+	ru := uint64(right)
+	switch tok {
+	case token.ADD:
+		return T(lu + ru)
+	case token.SUB:
+		return T(lu - ru)
+	case token.MUL:
+		return T(lu * ru)
+	case token.QUO:
+		return T(lu / ru)
+	case token.REM:
+		return T(lu % ru)
+	case token.AND:
+		return T(lu & ru)
+	case token.OR:
+		return T(lu | ru)
+	case token.XOR:
+		return T(lu ^ ru)
+	case token.SHL:
+		return T(lu << ru)
+	case token.SHR:
+		return T(lu >> ru)
+	case token.AND_NOT:
+		return T(lu &^ ru)
+	case token.EQL:
+		return lu == ru
+	case token.LSS:
+		return lu < ru
+	case token.GTR:
+		return lu > ru
+	case token.NEQ:
+		return lu != ru
+	case token.LEQ:
+		return lu <= ru
+	case token.GEQ:
+		return lu >= ru
+	default:
+		panic("unsupported token for unsigned integers: " + tok.String())
+	}
+}
+
+func floatOp[T floatNumber](tok token.Token, left, right T) any {
+	switch tok {
+	case token.ADD:
+		return left + right
+	case token.SUB:
+		return left - right
+	case token.MUL:
+		return left * right
+	case token.QUO:
+		return left / right
+	case token.EQL:
+		return left == right
+	case token.LSS:
+		return left < right
+	case token.GTR:
+		return left > right
+	case token.NEQ:
+		return left != right
+	case token.LEQ:
+		return left <= right
+	case token.GEQ:
+		return left >= right
+	default:
+		panic("unsupported token for floats: " + tok.String())
+	}
+}
+
+func stringOp(tok token.Token, left, right string) any {
+	switch tok {
+	case token.ADD:
+		return left + right
+	case token.EQL:
+		return left == right
+	case token.LSS:
+		return left < right
+	case token.GTR:
+		return left > right
+	case token.NEQ:
+		return left != right
+	case token.LEQ:
+		return left <= right
+	case token.GEQ:
+		return left >= right
+	default:
+		panic("unsupported token for strings: " + tok.String())
+	}
+}
+
+func assertBinaryFunc(t *testing.T, typeName string, tok token.Token, left, right any, want any) {
+	t.Helper()
+	key := typeName + strconv.Itoa(int(tok)) + typeName
+	fn, ok := binFuncs[key]
+	if !ok {
+		t.Fatalf("missing binary func for %s", key)
+	}
+	got := fn(reflect.ValueOf(left), reflect.ValueOf(right)).Interface()
+	if got != want {
+		t.Fatalf("unexpected result for %s (%v %s %v): got %v want %v", key, left, tok, right, got, want)
+	}
 }
